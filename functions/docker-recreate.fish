177ae33fc34be8079bf5c4b90f17bc8df727e9e4
Miroslav Shubernetskiy <miroslav@miki725.com>
2020-07-27 16:41:38 -0400

    removing volumes in docker-recreate

--- functions/docker-recreate.fish
+++ functions/docker-recreate.fish
@@ -1,5 +1,9 @@
 function docker-recreate
+    set -l volumes (docker inspect (docker-compose ps -q $argv) | jq -r '.[0].Mounts[].Name')
 	docker-compose kill $argv
 	docker-compose rm -f
+    for i in $volumes
+        docker volume rm $i
+    end
 	docker-compose up -d $argv
 end
